4cb42403415211e71e79e526bcd68abf977a6c7a
Alexey <marginalex2001@gmail.com>
2023-02-16 11:35:14 +0300

    first
    	new file:   hello.c

---
+++ hello.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+void menu();
+void name();
+void hi();
+
+int main()
+{
+  printf("NNNN\n");
+  printf("Hi\n");
+  printf("Hi man\n");
+  printf("Hi man\n");
+  int a = 3;
+  printf("+---+----+\n");
+  printf("|%3d|%4d|\n", a, 550);
+  printf("+---+----+\n");
+  menu();
+  printf("hi");
+  hi();
+  name();
+  return 0;
+}
+
+void menu()
+{
+    printf("+-----------+\n");
+    printf("|press Enter|\n");
+    printf("+-----------+\n");
+}
+
+void name()
+{
+	printf("hi");
+}
+
+void hi()
+{
+	getchar();
+}
