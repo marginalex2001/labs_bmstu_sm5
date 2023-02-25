b92923095181cfba7d664c0a4494db6d69d2e73e
Alexey <marginalex2001@gmail.com>
2023-02-25 11:09:50 +0300

    v1.1-beta

---
+++ laba1/table.cpp
@@ -0,0 +1,14 @@
+#include "table.h"
+#include "flight.h"
+#include <iostream>
+#include <ostream>
+
+
+using namespace std;
+
+void table(Flight)
+{
+	cout << "\n\n\n" << endl;
+	cout << Flight::
+	cout << "\n\n\n" << endl;
+}
---
+++ laba1/table.h
@@ -0,0 +1,7 @@
+#ifndef TABLE_H
+#define TABLE_H
+#include "flight.h"
+
+void table(Flight);
+
+#endif
