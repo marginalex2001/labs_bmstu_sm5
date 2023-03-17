f5a6b1aae5f905e4ab2a36525c552e42842dfc4b
Alexey <marginalex2001@gmail.com>
2023-03-17 16:24:32 +0300

    v2.4

--- laba2/main.cpp
+++ laba2/main.cpp
@@ -1,23 +1,98 @@
 #include "vector.h"
+//#include "menu.h"
+#include <iostream>
+
+using namespace std;
+
+void underline();
 
 int main()
 {
-	Vector vector;
-	Vector vector2(2);
-	// Vector vector3(vector2);
-	// vector.GetVector();
-	// vector2.GetVector();
-	// vector3.GetVector();
+	/*int i;
+	int counter = 0;
+	Vector arr[100];
+	do
+	{
+		i = menu();
+		switch (i)
+		{
+		case 1:
+			//Vector vector;
+			//arr[counter] = vector;
+			arr[counter] = Vector();
+
+			break;
+		case 4:
+			cout << "Enter number of elem" << endl;
+			int num;
+			cin >> num;
+			arr[num - 1].GetVector();
+			cout << endl;
+		case 0:
+			break;
+
+		default:
+			break;
+		}
+	} while (i != 0);*/
+	underline();
+	cout << "/            Start programm            /" << endl;
+	underline();
+
+	cout << "Create default vector" << endl;
+	Vector vector1;
+
+	underline();
+
+	int counter;
+	cout << "Create vector with a custom number of elements\n" << endl << "Enter number of elements: ";
+	cin.exceptions(cin.failbit);
+	try {
+		cin >> counter;
+	}
+	catch (const exception& ex) {
+		cout << "\033[31m!!!!!!!!!!!!!" << endl << ex.what() << endl;
+		cout << "\nError!!!" << endl << "Enter value again!!!" << endl << "!!!!!!!!!!!!!\033[0m\n" << endl;
+		cin.clear();
+		cin.ignore(10, '\n');
+	}
+	Vector vector2(counter);
+	cout << "\nSet vector2: " << endl;
 	vector2.SetVector();
-	// vector2.SortVector();
-	// vector2.GetVector();
+	cout << endl;
 
-	// vector2 = vector2 + vector;
-	vector2.GetVector();
-	vector.GetVector();
-	vector2 = vector2 - vector;
-	vector2.GetVector();
-	vector.GetVector();
+	underline();
 
+	cout << "Copy vector2 to vector3" << endl;
+	Vector vector3(vector2);
+
+	underline(); underline();
+
+	cout << "Vector 1: "; vector1.GetVector(); cout << endl;
+	cout << "Vector 2: "; vector2.GetVector(); cout << endl;
+	cout << "Vector 3: "; vector3.GetVector(); cout << endl;
+
+	underline();
+	cout << "\n\n" << endl;
+
+	cout << "vector2 = vector2 - vector1 :" << endl;
+	cout << "vector2 before:" << endl;
+	vector2.GetVector();
+	vector2 = vector2 - vector1;
+	cout << "vector2 after:" << endl;
+	vector2.GetVector();
+	underline();
+	cout << "Sort vector2 :" << endl;
+	cout << "vector2 before:" << endl;
+	vector2.GetVector();
+	vector2.SortVector();
+	cout << "vector2 after:" << endl;
+	vector2.GetVector();
+	
 	return 0;
+}
+
+void underline()
+{
+	cout << "/--------------------------------------/" << endl;
 }
--- laba2/vector.cpp
+++ laba2/vector.cpp
@@ -12,51 +12,53 @@ Vector::Vector()
 {
 	cout << "By default" << endl;
 	n = 3;
-	this->vector = new int [n];
-	for (int i = 0; i < 3; i++) 
+	this->vector = new int[n];
+	for (int i = 0; i < 3; i++)
 	{
 		vector[i] = 1;
 	}
 }
 
-Vector::Vector(const int &amount_)
+Vector::Vector(const int& amount_)
 {
 	n = amount_;
 	cout << "indicating the number of elements" << endl;
 	this->vector = new int[n];
 }
 
-Vector::Vector(Vector &vector_)
+Vector::Vector(Vector& vector_)
 {
+	cout << "Copy constr" << endl;
 	n = vector_.n;
 	this->vector = new int[n];
-	for (int i = 0; i < n; i++) 
+	for (int i = 0; i < n; i++)
 	{
-	    this->vector[i] = vector_.vector[i];
+		this->vector[i] = vector_.vector[i];
 	}
 }
 
 void Vector::SetVector()
 {
-	for (int i = 0; i < n; i++) 
+	for (int i = 0; i < n; i++)
 	{
 		cin.exceptions(cin.failbit);
 		try {
-	    	cout << "Enter " << i+1 << " value" << endl; 
-		    cin >> vector[i];
-		} catch (const exception& ex) {
-			 cout << "\033[31m!!!!!!!!!!!!!" << endl << ex.what() << endl;
-			 cout << "\nError!!!" << endl << "Enter value again!!!" << endl << "!!!!!!!!!!!!!\033[0m\n" << endl;
-			 cin.clear();
-			 cin.ignore(10, '\n');
-			 i--;
+			cout << "Enter " << i + 1 << " element" << endl;
+			cin >> vector[i];
+		}
+		catch (const exception& ex) {
+			cout << "\033[31m!!!!!!!!!!!!!" << endl << ex.what() << endl;
+			cout << "\nError!!!" << endl << "Enter value again!!!" << endl << "!!!!!!!!!!!!!\033[0m\n" << endl;
+			cin.clear();
+			cin.ignore(10, '\n');
+			i--;
 		}
 	}
 }
 
 void Vector::GetVector() const
 {
-	for (int i = 0; i < n; i++) 
+	for (int i = 0; i < n; i++)
 	{
 		cout << "[" << this->vector[i] << "]";
 	}
@@ -70,119 +72,76 @@ int Vector::GetSizeVector() const
 
 void Vector::SortVector()
 {
-	int valVect = sizeof(this->vector);
-	int valSize = sizeof(this->vector[0]);
-	int val = valVect / valSize;
-	std::sort(this->vector, this->vector + val);
+	std::sort(this->vector, this->vector + n);
 }
 
-Vector Vector::operator+(Vector &vector_)
+Vector& Vector::operator-(const Vector& vector_)
 {
-	int val_n = this->n;
-	int val_n_ = vector_.n;
+	if (this->n == vector_.n)
+	{
+		for (int i = 0; i < this->n; i++)
+		{
+			this->vector[i] = this->vector[i] - vector_.vector[i];
+		}
 
-	if (val_n < val_n_) 
-	{
-		val_n = val_n_;
-	}
-	if (val_n > val_n_)
-	{
-		val_n_ = val_n;
-		val_n = val_n_;
+		return *this;
 	}
 
-	Vector temp(val_n);
-	if (this->n < vector_.n)
+	bool vect_ShorterThis = this->n > vector_.n ? true : false;
+
+	if (vect_ShorterThis == false)
 	{
-		for (int i = 0; i < this->n; i++) 
+		static Vector temp(n);
+		for (int i = 0; i < this->n; i++)
 		{
 			temp.vector[i] = this->vector[i];
 		}
-		for (int i = this->n; i < vector_.n; i++) 
+		delete [] this->vector;
+		this->vector = new int[vector_.n];
+		this->n = vector_.n;
+		for (int i = 0; i < temp.n; i++)
 		{
-			temp.vector[i] = 1;
+			this->vector[i] = temp.vector[i];
 		}
-		for (int i = 0; i < this->n; i++) 
+		for (int i = temp.n; i < this->n; i++)
 		{
-			temp.vector[i] = this->vector[i] + vector_.vector[i];
+			this->vector[i] = 1;
 		}
+
+		for (int i = 0; i < this->n; i++)
+		{
+			this->vector[i] = this->vector[i] - vector_.vector[i];
+		}
+
+		return *this;
 	}
-	if (vector_.n < this->n)
+
+	if (vect_ShorterThis == true)
 	{
-		for (int i = 0; i < vector_.n; i++) 
+		static Vector temp(this->n);
+		for (int i = 0; i < vector_.n; i++)
 		{
 			temp.vector[i] = vector_.vector[i];
 		}
-		for (int i = vector_.n; i < val_n; i++ ) 
+		
+		for (int i = vector_.n; i < this->n; i++)
 		{
 			temp.vector[i] = 1;
 		}
-		for (int i = 0; i < this->n; i++) 
+
+		for (int i = 0; i < this->n; i++)
 		{
-			temp.vector[i] = temp.vector[i] + this->vector[i];// + vector_.vector[i];
+			this->vector[i] = this->vector[i] - temp.vector[i];
 		}
 
+		return *this;
 	}
-
-		return temp;
+	return *this;
+	
 }
 
-Vector Vector::operator-(Vector &vector_)
+Vector::~Vector()
 {
-	// int val_n = this->n;
-	// int val_n_ = vector_.n;
-	//
-	// if (val_n < val_n_) 
-	// {
-	// 	val_n = val_n_;
-	// }
-	// if (val_n > val_n_)
-	// {
-	// 	val_n_ = val_n;
-	// 	val_n = val_n_;
-	// }
-	//
-	// Vector temp(val_n);
-	// if (this->n < vector_.n)
-	// {
-	// 	for (int i = 0; i < this->n; i++) 
-	// 	{
-	// 		temp.vector[i] = this->vector[i];
-	// 	}
-	// 	for (int i = this->n; i < vector_.n; i++) 
-	// 	{
-	// 		temp.vector[i] = 1;
-	// 	}
-	// 	cout << temp.vector[0] << temp.vector[1] << temp.vector[2] << endl;
-	// 	for (int i = 0; i < this->n; i++) 
-	// 	{
-	// 		temp.vector[i] = temp.vector[i] - vector_.vector[i];// - vector_.vector[i];
-	// 		cout << temp.vector[0] << temp.vector[1] << temp.vector[2] << endl;
-	// 		cout << this->vector[0] << this->vector[1] << this->vector[2] << endl;
-	// 	}
-	// }
-	// if (vector_.n < this->n)
-	// {
-	// 	for (int i = 0; i < vector_.n; i++) 
-	// 	{
-	// 		temp.vector[i] = vector_.vector[i];
-	// 	}
-	// 	for (int i = vector_.n; i < val_n; i++ ) 
-	// 	{
-	// 		temp.vector[i] = 1;
-	// 		cout << vector_.n << endl;
-	// 	}
-	// 	for (int i = 0; i < val_n; i++) 
-	// 	{
-	// 		temp.vector[i] = this->vector[i] + vector_.vector[i];
-	// 	}
-	//
-	// }
-	Vector temp(this->n);
-	// Vector temp_(vector_.n);
-	for (int i = 0; i < this->n; i++) 
-	{
-		temp.vector[i] = this->vector[i] - vector_.vector[i];
-	}
-	return temp;
+	cout << "DESTRUCT" << endl;
+	delete[] vector;
 }
